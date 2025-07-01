74828eca948a8ded659d6b2ceb39079b449451ea
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-01 16:58:05 -0300

    refactor: update tests

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -2,6 +2,12 @@ import { Uuid } from "../../../shared/domain/value-objects/uuid.vo"
 import { Category } from "../category.entity"
 
 describe('Category Unit Tests', () => {
+
+    let validateSpy: any;
+    beforeEach(() => {
+        validateSpy = jest.spyOn(Category, "validate")
+    })
+
     test('Constructor', () => {
         const category = new Category({
             name: 'Movie'
@@ -12,5 +18,18 @@ describe('Category Unit Tests', () => {
         expect(category.description).toBeNull()
         expect(category.is_active).toBeTruthy()
         expect(category.created_at).toBeInstanceOf(Date)
+    })
+
+    describe("create command", () => {
+        test("should create a category", () => {
+            const category = Category.create({
+                name: "Movie"
+            })
+
+            expect(category.category_id).toBeInstanceOf(Uuid)
+            expect(category.name).toBe("Movie")
+            expect(category.description).toBeNull()
+            expect(validateSpy).toHaveBeenCalledTimes(1)
+        })
     })
 })

2e8cd167738e5de2b1b3c233597d141b8e09be5d
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-02 09:21:33 -0300

    test: Should insert a new entity in memory repository

---
+++ src/shared/db/in-memory/in-memory-repository.spec.ts
@@ -0,0 +1,52 @@
+import { Entity } from "../../domain/entity";
+import { Uuid } from "../../domain/value-objects/uuid.vo";
+import { InMemoryRepository } from "./in-memory.repository";
+
+type StubEntityConstructor = {
+    entity_id?: Uuid
+    name?: string
+    price: number
+}
+
+class StubEntity extends Entity {
+    entity_id: Uuid
+    name: string
+    price: number
+
+    constructor(props: StubEntityConstructor) {
+        super()
+        this.entity_id = props.entity_id || new Uuid()
+        this.name = props.name
+        this.price = props.price
+    }
+
+    toJSON() {
+        return {
+            entity_id: this.entity_id.id,
+            name: this.name,
+            price: this.price
+        }
+    }
+}
+
+class StubInMemoryRepository extends InMemoryRepository<StubEntity, Uuid> {
+    getEntity(): new (...args: any[]) => StubEntity {
+        return StubEntity
+    }
+}
+
+describe('InMemoryRepository Unit Tests', () => {
+    let repo: StubInMemoryRepository
+
+    beforeEach(() => {
+        repo = new StubInMemoryRepository()
+    })
+
+    test('Should insert a new entity', async () => {
+        const entity = new StubEntity({
+            entity_id: new Uuid(),
+            name: 'Test',
+            price: 100
+        })
+    })
+})

cd7f340422ebadfffeb3117c25139304682018f5
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-02 08:53:23 -0300

    refactor: update IRepository

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -3,10 +3,11 @@ import { ValueObject } from '../value-object'
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
     insert(entity: E): Promise<void>
+    bulkInsert(entities: E[]): Promise<void>
     update(entity: E): Promise<void>
     delete(entity: E): Promise<void>
 
-    findById(entity_id: EntityId): Promise<E>
+    findById(entity_id: EntityId): Promise<E | null>
     findAll(): Promise<E[]>
     
     getEntity(): new (...args: any[]) => E

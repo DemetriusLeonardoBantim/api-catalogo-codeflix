f8d7275c67fd4d72eb5b51c5715293ce05fe7b76
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-03 09:50:30 -0300

    feat: criar metodo de busca personalizado no repositorio em memoria

--- src/infra/db/in-memory/in-memory.repository.ts
+++ src/infra/db/in-memory/in-memory.repository.ts
@@ -1,53 +1,161 @@
+import { Entity } from '../../../shared/domain/entity';
+import { NotFoundError } from '../../../shared/domain/errors/not-found.error';
+import { IRepository, ISearchableRepository } from '../../../shared/domain/repository/repository-interface';
 
-import { Entity } from "../../../shared/domain/entity";
-import { NotFoundError } from "../../../shared/domain/errors/not-found.error";
-import { IRepository } from "../../../shared/domain/repository/repository-interface";
-import { ValueObject } from "../../../shared/domain/value-object";
+import {
+  SearchParams,
+  SortDirection,
+} from '../../../shared/domain/repository/search-params';
+import { SearchResult } from '../../../shared/domain/repository/search-result';
+import { ValueObject } from '../../../shared/domain/value-object';
 
-export abstract class InMemoryRepository <E extends Entity, EntityId extends ValueObject> implements IRepository<E, EntityId> {
+export abstract class InMemoryRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+> implements IRepository<E, EntityId>
+{
+  items: E[] = [];
 
-    items: E[] = []
-    async insert(entity: any): Promise<void> {
-       this.items.push(entity)
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity);
+  }
+  async bulkInsert(entities: any[]): Promise<void> {
+    this.items.push(...entities);
+  }
+
+  async update(entity: E): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity.entity_id),
+    );
+    if (indexFound === -1) {
+      throw new NotFoundError(entity.entity_id, this.getEntity());
+    }
+    this.items[indexFound] = entity;
+  }
+
+  async delete(entity_id: EntityId): Promise<void> {
+    const indexFound = this.items.findIndex((item) =>
+      item.entity_id.equals(entity_id),
+    );
+    if (indexFound === -1) {
+      throw new NotFoundError(entity_id, this.getEntity());
+    }
+    this.items.splice(indexFound, 1);
+  }
+
+  async findById(entity_id: EntityId): Promise<E | null> {
+    const item = this.items.find((item) => item.entity_id.equals(entity_id));
+    return typeof item === 'undefined' ? null : item;
+  }
+
+  async findAll(): Promise<any[]> {
+    return this.items;
+  }
+
+  async findByIds(ids: EntityId[]): Promise<E[]> {
+    //avoid to return repeated items
+    return this.items.filter((entity) => {
+      return ids.some((id) => entity.entity_id.equals(id));
+    });
+  }
+
+  async existsById(
+    ids: EntityId[],
+  ): Promise<{ exists: EntityId[]; not_exists: EntityId[] }> {
+    if (!ids.length) {
+
     }
 
-    async bulkInsert(entities: any[]): Promise<void> {
-       this.items.push(...entities)
+    if (this.items.length === 0) {
+      return {
+        exists: [],
+        not_exists: ids,
+      };
     }
 
-    async update(entity: E): Promise<void> {
-        const indexFound = this.items.findIndex((item) => {
-            item.entity_id.equals(entity.entity_id)
-        }) 
-        if(indexFound === -1) {
-            throw new NotFoundError(entity.entity_id, this.getEntity())
-        }
-        this.items[indexFound] = entity
+    const existsId = new Set<EntityId>();
+    const notExistsId = new Set<EntityId>();
+    ids.forEach((id) => {
+      const item = this.items.find((entity) => entity.entity_id.equals(id));
+      item ? existsId.add(id) : notExistsId.add(id);
+    });
+    return {
+      exists: Array.from(existsId.values()),
+      not_exists: Array.from(notExistsId.values()),
+    };
+  }
+
+  abstract getEntity(): new (...args: any[]) => E;
+}
+
+export abstract class InMemorySearchableRepository<
+    E extends Entity,
+    EntityId extends ValueObject,
+    Filter = string,
+  >
+  extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>
+{
+  sortableFields: string[] = [];
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sort_dir,
+    );
+    const itemsPaginated = this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.per_page,
+    );
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page,
+    });
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: Filter | null,
+  ): Promise<E[]>;
+
+  protected applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    per_page: SearchParams['per_page'],
+  ) {
+    const start = (page - 1) * per_page; // 0 * 15 = 0
+    const limit = start + per_page; // 0 + 15 = 15
+    return items.slice(start, limit);
+  }
+
+  protected applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: SortDirection | null,
+    custom_getter?: (sort: string, item: E) => any,
+  ) {
+    if (!sort || !this.sortableFields.includes(sort)) {
+      return items;
     }
 
-    async delete(entity_id: any): Promise<void> {
-        const indexFound = this.items.findIndex((item) => {
-            item.entity_id.equals(entity_id)
-        })
-        if(indexFound === -1) {
-            throw new Error("Entity not found")
-        }
-        this.items.splice(indexFound, 1)
-    }
+    return [...items].sort((a, b) => {
+        // @ts-ignore
+      const aValue = custom_getter ? custom_getter(sort, a) : a[sort];
+        // @ts-ignore
+      const bValue = custom_getter ? custom_getter(sort, b) : b[sort];
+      if (aValue < bValue) {
+        return sort_dir === 'asc' ? -1 : 1;
+      }
 
-    findById(entity_id: any): Promise<E> {
+      if (aValue > bValue) {
+        return sort_dir === 'asc' ? 1 : -1;
+      }
 
-        return this._get(entity_id)
-    }
-
-    async findAll(): Promise<any[]> {
-        return this.items
-    }
-
-    async _get(entity_id: EntityId){
-        const item = this.items.find((item) => item.entity_id.equals(entity_id))
-        return typeof item === 'undefined' ? null : item;
-    }
-
-    abstract getEntity(): new (...args: any[]) => E;
+      return 0;
+    });
+  }
 }
--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -7,7 +7,7 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
     insert(entity: E): Promise<void>
     bulkInsert(entities: E[]): Promise<void>
     update(entity: E): Promise<void>
-    delete(entity: E): Promise<void>
+    delete(entity_id: EntityId): Promise<void>;
 
     findById(entity_id: EntityId): Promise<E | null>
     findAll(): Promise<E[]>
@@ -16,11 +16,12 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
 }
 
 export interface ISearchableRepository<
-    E extends Entity,
-    EntityId extends ValueObject,
-    SearchInput = SearchParams,
-    SearchOutput = SearchResult
+  E extends Entity,
+  EntityId extends ValueObject,
+  Filter = string,
+  SearchInput = SearchParams<Filter>,
+  SearchOutput = SearchResult,
 > extends IRepository<E, EntityId> {
-    sortableFields: string[]
-    search(props: SearchInput): Promise<SearchOutput>
+  sortableFields: string[];
+  search(props: SearchInput): Promise<SearchOutput>;
 }

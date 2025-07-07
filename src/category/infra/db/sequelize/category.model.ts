db7bab4e121835692ba3666990abcf64631c982a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-07 08:40:27 -0300

    feat: create model to Category

--- src/category/infra/db/sequelize/category.model.ts
+++ src/category/infra/db/sequelize/category.model.ts
@@ -1,5 +1,20 @@
-import { Model } from "sequelize-typescript";
+import { AllowNull, Column, DataType, Model, PrimaryKey } from "sequelize-typescript";
 
 export class CategoryModel extends Model {
     
+    @PrimaryKey
+    @Column({ type: DataType.UUID })
+    declare category_id: string
+    
+    @Column({ allowNull: false, type: DataType.STRING(255) })
+    declare name: string
+    
+    @Column({ allowNull: false, type: DataType.TEXT })
+    declare description: string | null
+    
+    @Column({ allowNull: false, type: DataType.BOOLEAN })
+    declare is_active: boolean
+    
+    @Column({ allowNull: false, type: DataType.DATE(3) })
+    declare created_at: Date
 }

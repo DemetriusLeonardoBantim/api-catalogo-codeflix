8b6ca57587dc0298b0bce07e761891150122547f
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-07 09:29:31 -0300

    test: create test integration in CategoryModel

--- src/category/infra/db/sequelize/category.model.int.spec.ts
+++ src/category/infra/db/sequelize/category.model.int.spec.ts
@@ -13,13 +13,14 @@ describe('CategoryModel Integration Tests', () => {
 
         await sequelize.sync({ force: true })
     
-        const teste = CategoryModel.create({
+        const CategoryModelObj = CategoryModel.create({
             category_id: 'a5d3e95e-3ac9-4a6f-baf8-8d27d7bb9131',
             name: 'teste',
             description: 'description teste',
             is_active: true,
             created_at: new Date()
         })
-        console.log(teste)
+
+        expect(CategoryModelObj).toBeTruthy()
     }) 
 })

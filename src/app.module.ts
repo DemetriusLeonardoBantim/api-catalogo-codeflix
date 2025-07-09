ddb439d29625a7c821cd714981d52411ef88cffe
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-09 10:29:44 -0300

    refactor: configure SequelizeModule  in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    CategoriesModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite' as any,
+      host: ':memory',
+      logging: false
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

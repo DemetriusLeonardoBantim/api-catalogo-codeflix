ed020653fa67f097b017f6e2add80524db7e55e3
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-09 12:54:26 -0300

    feat: use config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
     CategoriesModule,
     SequelizeModule.forRoot({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       dialect: 'sqlite' as any,
       host: ':memory',
-      logging: false
-    })
+      logging: false,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],

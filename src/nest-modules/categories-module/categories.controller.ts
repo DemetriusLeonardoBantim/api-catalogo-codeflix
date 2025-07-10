139275b69667cbc6f37d21da4b247bcdc915551a
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2025-07-10 14:40:13 -0300

    feat: create categires controller

---
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Inject,
+} from '@nestjs/common';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import {
+  CreateCategoryInput,
+  CreateCategoryUseCase,
+} from '@core/category/application/create-category.use-case';
+import { UpdateCategoryUseCase } from '@core/category/application/update-category.use-case';
+import { DeleteCategoryUseCase } from '@core/category/application/delete-category.use-case';
+import { GetCategoryUseCase } from '@core/category/application/get-category.use-case';
+import { CategoryPresenter } from './categories.presenter';
+
+@Controller('categories')
+export class CategoriesController {
+
+  @Inject(CreateCategoryUseCase)
+  private createUseCase: CreateCategoryUseCase;
+
+  @Inject(UpdateCategoryUseCase)
+  private updateUseCase: UpdateCategoryUseCase;
+
+  @Inject(DeleteCategoryUseCase)
+  private deleteUseCase: DeleteCategoryUseCase;
+
+  @Inject(GetCategoryUseCase)
+  private getUseCase: GetCategoryUseCase;
+
+  @Post()
+  async create(@Body() createCategoryDto: CreateCategoryInput) {
+    const output = await this.createUseCase.execute(createCategoryDto);
+    return CategoriesController.serialize(output)
+  }
+
+  @Get()
+  findAll() {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {}
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {}
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {}
+
+  static serialize(output: any) {
+    return new CategoryPresenter(output);
+  }
+}
